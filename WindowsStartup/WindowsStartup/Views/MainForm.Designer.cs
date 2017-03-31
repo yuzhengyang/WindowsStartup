8246c9d4cb52be31bde19863f0f56a3aead56582
yuzhengyang <inc@live.cn>
2017-03-31 16:48:22 +0800

    整理注册表、开始菜单和计划任务的开机启动方式

--- WindowsStartup/WindowsStartup/Views/MainForm.Designer.cs
+++ WindowsStartup/WindowsStartup/Views/MainForm.Designer.cs
@@ -35,15 +35,19 @@
             this.BtRegAdd = new System.Windows.Forms.Button();
             this.BtRegDel = new System.Windows.Forms.Button();
             this.TbCmd = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.BtTaskAdd = new System.Windows.Forms.Button();
+            this.BtTaskDel = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.BtMenuAdd);
             this.groupBox2.Controls.Add(this.BtMenuDel);
-            this.groupBox2.Location = new System.Drawing.Point(406, 80);
+            this.groupBox2.Location = new System.Drawing.Point(256, 371);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(187, 62);
             this.groupBox2.TabIndex = 8;
@@ -74,7 +78,7 @@
             // 
             this.groupBox1.Controls.Add(this.BtRegAdd);
             this.groupBox1.Controls.Add(this.BtRegDel);
-            this.groupBox1.Location = new System.Drawing.Point(406, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 371);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(187, 62);
             this.groupBox1.TabIndex = 7;
@@ -107,21 +111,55 @@
             this.TbCmd.Multiline = true;
             this.TbCmd.Name = "TbCmd";
             this.TbCmd.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.TbCmd.Size = new System.Drawing.Size(378, 331);
+            this.TbCmd.Size = new System.Drawing.Size(676, 341);
             this.TbCmd.TabIndex = 6;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.BtTaskAdd);
+            this.groupBox3.Controls.Add(this.BtTaskDel);
+            this.groupBox3.Location = new System.Drawing.Point(500, 371);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(187, 62);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "计划任务";
+            // 
+            // BtTaskAdd
+            // 
+            this.BtTaskAdd.Location = new System.Drawing.Point(13, 23);
+            this.BtTaskAdd.Name = "BtTaskAdd";
+            this.BtTaskAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtTaskAdd.TabIndex = 0;
+            this.BtTaskAdd.Text = "添加";
+            this.BtTaskAdd.UseVisualStyleBackColor = true;
+            this.BtTaskAdd.Click += new System.EventHandler(this.BtTaskAdd_Click);
+            // 
+            // BtTaskDel
+            // 
+            this.BtTaskDel.Location = new System.Drawing.Point(97, 23);
+            this.BtTaskDel.Name = "BtTaskDel";
+            this.BtTaskDel.Size = new System.Drawing.Size(75, 23);
+            this.BtTaskDel.TabIndex = 3;
+            this.BtTaskDel.Text = "删除";
+            this.BtTaskDel.UseVisualStyleBackColor = true;
+            this.BtTaskDel.Click += new System.EventHandler(this.BtTaskDel_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 355);
+            this.ClientSize = new System.Drawing.Size(700, 447);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.TbCmd);
             this.Name = "MainForm";
             this.Text = "设置程序自启动";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,5 +174,8 @@
         private System.Windows.Forms.Button BtRegAdd;
         private System.Windows.Forms.Button BtRegDel;
         private System.Windows.Forms.TextBox TbCmd;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button BtTaskAdd;
+        private System.Windows.Forms.Button BtTaskDel;
     }
 }
--- WindowsStartup/WindowsStartup/Views/MainForm.cs
+++ WindowsStartup/WindowsStartup/Views/MainForm.cs
@@ -1,22 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using WindowsStartup.Utils;
 
 namespace WindowsStartup.Views
 {
     public partial class MainForm : Form
     {
+        string AppName = "WindowsStartup";
+        string AppFile = Application.ExecutablePath;
         public MainForm()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
         }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
 
+        }
         private void UICmd(string s)
         {
             BeginInvoke(new Action(() =>
@@ -26,24 +27,83 @@ namespace WindowsStartup.Views
             }));
         }
 
+        #region 注册表的开机启动
         private void BtRegAdd_Click(object sender, EventArgs e)
         {
+            string regAll = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+            if (RegisterTool.SetValue(regAll, AppName, AppFile))
+                UICmd("添加 注册表全局用户启动 成功");
+            else
+                UICmd("添加 注册表全局用户启动 失败");
 
+            string regCurrent = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run";
+            if (RegisterTool.SetValue(regCurrent, AppName, AppFile))
+                UICmd("添加 注册表当前用户启动 成功");
+            else
+                UICmd("添加 注册表当前用户启动 失败");
         }
-
         private void BtRegDel_Click(object sender, EventArgs e)
         {
+            string regAll = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+            if (RegisterTool.DeleteValue(regAll, AppName))
+                UICmd("删除 注册表全局用户启动 成功");
+            else
+                UICmd("删除 注册表全局用户启动 失败");
 
+            string regCurrent = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run";
+            if (RegisterTool.DeleteValue(regCurrent, AppName))
+                UICmd("删除 注册表当前用户启动 成功");
+            else
+                UICmd("删除 注册表当前用户启动 失败");
         }
+        #endregion
 
+        #region 开始菜单启动项
         private void BtMenuAdd_Click(object sender, EventArgs e)
         {
+            UICmd("正在读取 全局用户开始菜单启动文件夹");
+            string commonStartup = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartup);
+            UICmd("CommonStartup：" + commonStartup);
+            if (ShortcutTool.Create(commonStartup, AppName, AppFile))
+                UICmd("添加 全局用户开始菜单启动 成功");
+            else
+                UICmd("添加 全局用户开始菜单启动 失败");
 
+            UICmd("正在读取 当前用户开始菜单启动文件夹");
+            string startup = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            UICmd("Startup：" + startup);
+            if (ShortcutTool.Create(startup, AppName, AppFile))
+                UICmd("添加 当前用户开始菜单启动 成功");
+            else
+                UICmd("添加 当前用户开始菜单启动 失败");
         }
-
         private void BtMenuDel_Click(object sender, EventArgs e)
         {
+            string commonStartup = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartup);
+            if (ShortcutTool.Delete(commonStartup, AppName))
+                UICmd("删除 全局用户开始菜单启动 成功");
+            else
+                UICmd("删除 全局用户开始菜单启动 失败");
 
+            string startup = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            if (ShortcutTool.Delete(startup, AppName))
+                UICmd("删除 当前用户开始菜单启动 成功");
+            else
+                UICmd("删除 当前用户开始菜单启动 失败");
+        }
+
+        #endregion
+
+        private void BtTaskAdd_Click(object sender, EventArgs e)
+        {
+            var task = TaskSchedulerTool.Create(AppName, AppFile, "Tom", "测试程序自启动计划任务");
+            UICmd("添加 计划任务启动 成功");
+        }
+
+        private void BtTaskDel_Click(object sender, EventArgs e)
+        {
+            TaskSchedulerTool.Delete(AppName);
+            UICmd("删除 计划任务启动 成功");
         }
     }
 }
